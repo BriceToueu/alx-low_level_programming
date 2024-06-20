b99c357054e312ecd268b693196c22dbbd056015
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-06-20 16:48:59 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints exactly the text of the last task on  hello world C project
+ *              followed by new line, to the standard error.
+ *
+ * Return: Alawys 1 (sucess)
+ */
+
+int main(void)
+
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
