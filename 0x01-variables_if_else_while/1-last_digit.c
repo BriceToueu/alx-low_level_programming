e33620f300d81bfa7e12ba659510c733c34fe153
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-06-21 09:25:07 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - Entry point
  *
  * Description: This program will assign a random number to the variable n
- * 		each time it is executed
+ *              each time it is executed
  *
  * Return: Always 0 (sucess)
  */
