e616e650656025fa038a10a804e350dce53a083b
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-06-21 19:19:23 -0700

    Write a program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -31,10 +31,10 @@ int main(void)
 
 			/* if condition for the two numbers */
 			if (numberOne == 98 && numberTwo == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+			{
+				putchar(',');
+				putchar(' ');
+			}
 
 			numberTwo++;
 		}
