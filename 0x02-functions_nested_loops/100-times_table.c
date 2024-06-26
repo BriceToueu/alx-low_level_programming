7f9f108eec71aa297d78721cb1092ba19675a3d7
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-06-26 09:33:42 -0700

    Write a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,7 @@ void print_times_table(int n)
 {
 	int number1 = 0, number2, product;
 
+
 	if (n >= 0 && n <= 15)
 	{
 		while (number1 <= n)
@@ -31,10 +32,10 @@ void print_times_table(int n)
 
 				if (product >= 100)
 				{
-					_putchar((productn/ 100) + '0');
-					_putchar(((product /10)) % 10 + '0');
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
 				}
-				else if(product <= 99 && product >= 10)
+				else if (product <= 99 && product >= 10)
 				{
 					_putchar((product / 10) + '0');
 				}
