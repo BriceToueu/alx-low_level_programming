7946b9326b17829a80ffa76491b8871b20ec9f6d
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-06-27 04:40:24 -0700

    You are not coding the solution / function, you’re just testing it!

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,23 +7,50 @@
  */
 
 int main(void)
+
 {
-	int count = 0;
-	unsigned long fib1 = 1, fib2 = 2;
-	unsigned long next;
+        int count = 0;
+        unsigned long fib1 = 0, fib2 = 1, sum;
+        unsigned long fib1_mi1, fib1_mi2, fib2_mi1, fib2_mi2;
+        unsigned long mi1, mi2;
 
-	printf("%lu, %lu", fib1, fib2);
+        while (count < 92)
+        {
+                sum = fib1 + fib2;
+                printf("%lu, ", sum);
 
-	while (count < 96)
-	{
-		next = fib1 + fib2;
-		printf(", %lu", next);
+                fib1 = fib2;
+                fib2 = sum;
+                count++;
+        }
 
-		fib1 = fib2;
-		fib2 = next;
-		count++;
-	}
+        fib1_mi1 = fib1 / 10000000000;
+        fib2_mi1 = fib2 / 10000000000;
+        fib1_mi2 = fib1 % 10000000000;
+        fib2_mi1 = fib2 % 10000000000;
 
-	printf("\n");
-	return (0);
+        count = 93;
+        while (count < 99)
+        {
+                mi1 = fib1_mi1 + fib2_mi1;
+                mi2 = fib1_mi2 + fib2_mi2;
+                if (fib1_mi2 + fib2_mi2 > 9999999999)
+                {
+                        mi1 += 1;
+                        mi2 %= 10000000000;
+                }
+
+                printf("%lu%lu", mi1, mi2);
+                if (count != 98)
+                        printf(", ");
+
+                fib1_mi1 = fib2_mi1;
+                fib1_mi2 = fib2_mi2;
+                fib2_mi1 = mi1;
+                fib2_mi2 = mi2;
+                count++;
+        }
+        printf("\n");
+
+        return (0);
 }
