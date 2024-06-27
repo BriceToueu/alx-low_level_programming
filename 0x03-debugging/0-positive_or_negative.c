e611bff49e82f2fdcf1fa6d63862259f1417f3db
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-06-27 05:51:45 -0700

    You are not coding the solution / function, you’re just testing it!

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program generates a random number and determines
+ *              whether it is positive, negative or zero
+ *
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0)); /* Initializes the random number generator */
+			/* with a seed based on the current time */
+
+	n = rand() - RAND_MAX / 2; /* Generates a random number between */
+					/* -RAND_MAX/2 and RAND_MAX/2 */
+	printf("%d", n);
+
+	if (n > 0)
+		printf(" is positive\n");
+	else if (n == 0)
+		printf(" is zero\n");
+	else
+		printf(" is negative\n");
+
+	return (0);
+}
