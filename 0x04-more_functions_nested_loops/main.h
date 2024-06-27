135afad4e1a5a0650187fe2d9d26abfaa8aa69a3
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-06-27 07:00:45 -0700

    You are not coding the solution / function, you’re just testing it!

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Brice Toueu
+ * Description: Header file containing declarations for all functions
+ *              used in the 0x02-functions_nested_loops directory.
+ */
+
+int _abs(int);
+int add(int, int);
+int _isalpha(int c);
+int _islower(int c);
+void jack_bauer(void);
+int _putchar(char c);
+int print_sign(int n);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int print_last_digit(int);
+void print_times_table(int n);
+void print_to_98(int n);
+void times_table(void);
+
+
+#endif
