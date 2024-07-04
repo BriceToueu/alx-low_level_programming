5b0bedbd19bae24176bbcd963562129576b36372
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-07-04 09:33:36 -0700

    Write a function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,27 @@
 #include "main.h"
-
 /**
- * _strncat - copies a string
- * @dest: string to be copied to
- * @src: string to copy
- * @n: no. of bytes
- *
- * Return: dest
+ *  _strncpy - C function that copies a string, including the
+ *  terminating null byte, using at most an inputted number of bytes.
+ *  If the length of the source string is less than the maximum byte number,
+ *  the remainder of the destination string is filled with null bytes.
+ *  Works identically to the standard library function `strncpy`.
+ *@dest: buffer storing the string copy
+ *@src:the source string
+ *@n:max nummber of byte copied
+ *Return: returns
  */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
-	int i = 0;
+	int i;
 
-	/* Cps characters from src to dest as long as i < n and src[i] is not '\0' */
+	i = 0;
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	/* Fills the rest of dest with null characters ('\0') until n is reached */
+
 	while (i < n)
 	{
 		dest[i] = '\0';
