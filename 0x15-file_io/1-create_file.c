f4d1d80d8da2966b389b92b4fdbc167d94345db0
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-08-12 13:42:53 -0700

    Create a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - A function that creates a file
+ * @filename: The filename to create
+ * @text_content: A NULL terminated string to write to the file
+ * Return: 1 on success, -1 if file can not be created, nor written,
+ * nor write fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t written;
+	size_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+
+		written = write(fd, text_content, len);
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
